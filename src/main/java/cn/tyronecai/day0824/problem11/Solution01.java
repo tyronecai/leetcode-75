d50d1c93a633d3a69f76f08288268c91006458ac
cai.rong <cai.rong@yottabyte.cn>
2023-08-24 10:37:55 +0800

    20230824

--- src/main/java/cn/tyronecai/day0824/problem11/Solution01.java
+++ src/main/java/cn/tyronecai/day0824/problem11/Solution01.java
@@ -1,5 +1,66 @@
-package cn.tyronecai.day0824.problem11;/**
+package cn.tyronecai.day0824.problem11;
+
+import com.google.common.io.Resources;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.lang.reflect.Parameter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
  * @author cai.rong@yottabyte.cn
  * @date 2023-08-24 09:12
- */public class Solution01 {
+ *
+ * https://leetcode.cn/problems/container-with-most-water/?envType=study-plan-v2&envId=leetcode-75
+ * 11. 盛最多水的容器
+ * 提示
+ * 中等
+ * 4.5K
+ * 相关企业
+ * 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+ *
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ *
+ * 返回容器可以储存的最大水量。
+ *
+ * 说明：你不能倾斜容器。
+ */
+public class Solution01 {
+
+    public int maxArea(int[] height) {
+        int result = 0;
+        int startIndex = 0;
+        int endIndex = 0;
+
+        // 暴力硬算
+        for (int i = 0; i < height.length; i++) {
+            int num = height[i];
+            for (int j = i+1; j < height.length; j++) {
+                int newArea = Math.min(num, height[j]) * (j - i);
+                if (newArea > result) {
+                    result = newArea;
+                    startIndex = i;
+                    endIndex = j;
+                }
+            }
+        }
+        System.out.printf("start %s, end %s, area %s %n", startIndex, endIndex, result);
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String[] input = new String(Files.readAllBytes(Paths.get(Resources.getResource("day0824/problem11/input.txt").getPath()))).split(",");
+
+        int[] height = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            height[i] = Integer.parseInt(input[i]);
+        }
+        System.out.println(height.length);
+
+        int result = new Solution01().maxArea(height);
+        System.out.println(result);
+    }
 }
--- src/main/java/cn/tyronecai/day0824/problem11/Solution02.java
+++ src/main/java/cn/tyronecai/day0824/problem11/Solution02.java
@@ -1,5 +1,51 @@
-package cn.tyronecai.day0824.problem11;/**
+package cn.tyronecai.day0824.problem11;
+
+import com.google.common.io.Resources;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
  * @author cai.rong@yottabyte.cn
  * @date 2023-08-24 09:58
- */public class Solution02 {
+ */
+public class Solution02 {
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+        int result = 0;
+
+        // 在每个状态下，无论长板或短板向中间收窄一格，都会导致水槽 底边宽度  变短：
+        //
+        //若向内 移动短板 ，水槽的短板 min(h[i],h[j])min(h[i], h[j])min(h[i],h[j]) 可能变大，因此下个水槽的面积 可能增大 。
+        //若向内 移动长板 ，水槽的短板 min(h[i],h[j])min(h[i], h[j])min(h[i],h[j]) 不变或变小，因此下个水槽的面积 一定变小 。
+        //因此，初始化双指针分列水槽左右两端，循环每轮将短板向内移动一格，并更新面积最大值，直到两指针相遇时跳出；即可获得最大面积。
+        while (i < j) {
+            int area = Math.min(height[i], height[j]) * (j - i);
+            result = Math.max(result, area);
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+
+        System.out.printf("max area %s %n", result);
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String[] input = new String(Files.readAllBytes(Paths.get(Resources.getResource("day0824/problem11/input.txt").getPath()))).split(",");
+
+        int[] height = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            height[i] = Integer.parseInt(input[i]);
+        }
+        System.out.println(height.length);
+
+        int result = new Solution02().maxArea(height);
+        System.out.println(result);
+    }
 }
